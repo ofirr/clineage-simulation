f34c98134a4362fb470b7fb55995c7caade21149
hisplan <jaeyoung.chun@gmail.com>
2018-05-31 14:16:52 +0300

    Add mutation noise

--- run.m
+++ run.m
@@ -1,6 +1,8 @@
-% clean up from any previous work
+%% clean up from any previous work
 clear;
 clc;
+
+%% initialize
 
 % path to eSTGt
 addpath('C:\Users\chun\projects\eSTGt\eSTGt');
@@ -18,6 +20,8 @@ path_mutation_table = fullfile(path_output, 'mutation_table.txt');
 path_newick = fullfile(path_output, 'tree.newick');
 path_tree_png = fullfile(path_output, 'tree.png');
 
+%% simulation
+
 % load microsatellite mutation transition table
 % global varaible so that it can be accessed from eSTGt
 load('ms_mutation_transition_prob');
@@ -32,8 +36,18 @@ num_of_ms_loci = rules.Prod{1,1}.InternalStates.MS.DupNum;
 % run simulation
 [ runs, RunsData ] = RunSim(rules, rules.Seed, rules.SimTime);
 
+%% generate mutation table and tree
+
 % the very first run (we only have one run in fact)
 my_run = runs(1);
+
+% create tree
+create_tree(...
+    rules, ...
+    my_run, ...
+    path_newick, ...
+    path_tree_png ...
+);
 
 % create mutation table
 mutation_table = create_mutation_table(...
@@ -45,20 +59,26 @@ mutation_table = create_mutation_table(...
 % display to screen
 disp(mutation_table);
 
-% create tree
-create_tree(...
-    rules, ...
-    my_run, ...
-    path_newick, ...
-    path_tree_png ...
-);
-
-% load allelic dropout probability table
-load('allelic_dropout_prob');
-
 % get number of samples, number of microsatellite loci
 num_of_samples = size(mutation_table, 1);
 num_of_ms_loci = size(mutation_table, 2);
+
+% add mutation noise
+mutation_noise_threshold = 0.00194622849;
+
+mutation_noise_table = generate_mutation_noise_table(...
+    num_of_samples, ...
+    num_of_ms_loci, ...
+    mutation_noise_threshold ...
+);
+
+mutation_table{:,:} = mutation_table{:,:} + mutation_noise_table;
+
+% display to screen
+disp(mutation_table);
+
+% load allelic dropout probability table
+load('allelic_dropout_prob');
 
 % get allelic dropout truth table
 % 0: don't drop, 1: drop
