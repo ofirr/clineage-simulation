699e7c0e9dee5bc63614cf87b579111edea4f7a9
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2018-06-06 21:44:13 +0300

    Fix bug, remove unnecessary

--- update_microsatellite.m
+++ update_microsatellite.m
@@ -2,9 +2,15 @@ function [ newMS ] = update_microsatellite( MS )
 % updates the microsatellite repeat number
 
     rand_seed = RandStream('mlfg6331_64');
+    
+    % access to simulation options
+    global simul_options;
 
     % access to microsatellite mutation transition table
     global ms_mutation_transition_prob;
+    
+    % mapping between index and ms repeat length
+    global ms_repeat_lengths;
     
     % check if MS has all repeat number -1, which means it requires
     % reinitialization
@@ -18,15 +24,28 @@ function [ newMS ] = update_microsatellite( MS )
         
     end
     
+    % don't cause mutations if the option is turned off
+    if ~simul_options.add_mutation
+        newMS = MS;
+        return;
+    end
+    
     for i = 1:length(MS)
 
         try
-            probs = ms_mutation_transition_prob(MS(i), :);
+            % convert ms repeat length to index
+            idx = find(ms_repeat_lengths == MS(i));
+            
+            % using the index, get probability distribution
+            probs = ms_mutation_transition_prob(idx, :);
         catch
             fprintf("error: %d\n", i);
         end        
         
         %fixme: with rand_seed, barely changes
+        
+        % get index that corresponds to ms repeat length using probability
+        % distribution
         new_idx = datasample(...
             1:length(probs), ...
             1, ...
@@ -34,20 +53,9 @@ function [ newMS ] = update_microsatellite( MS )
             'Weights', probs ...
         );
         
-        newMS(i) = new_idx;
+        % convert index to ms repeat length
+        newMS(i) = ms_repeat_lengths(new_idx);
         
     end
 
-    return;
-
-
-
-    Mu = 1/1000; % the mutation rate (10^-3)
-
-    n = length(MS);
-    MutateVector = binornd(1,Mu,1,n);
-    MutDirection = binornd(MutateVector, 1/2);
-    newMS = MS + (2*MutDirection - MutateVector);
-
 end
-
