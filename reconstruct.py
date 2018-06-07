697432764fb614781f9bb3d74173f1867fc7db40
hisplan <jaeyoung.chun@gmail.com>
2018-06-07 08:34:30 +0300

    Revise

--- reconstruct.py
+++ reconstruct.py
@@ -46,12 +46,15 @@ run_sagis_triplets(
     textual_mutation_dict=tcalling,
     cells_to_be_used_as_root=rldr,
     newick_tree_path=triplets_tree_path,
-    tripletsnumber=5000000, 
+    tripletsnumber=5000000,
     score_threshold=0,
     scoring_method='uri10',
     loci_filter='ncnr')
 
-tree = dendropy.Tree.get_from_path(
+# fixme: would be nice if we can do this when calling `run_sagis_triplets`
+os.rename("treeReconstruction.log", os.path.join(work_path, "sagis_triplets.log"))
+
+tree_reconstructed = dendropy.Tree.get_from_path(
     triplets_tree_path,
     "newick"
 )
@@ -60,15 +63,20 @@ tree = dendropy.Tree.get_from_path(
 # root_node = tree.find_node_with_taxon_label('root')
 # tree.prune_subtree(root_node);
 
-print(tree.as_ascii_plot())
+with open(os.path.join(work_path, 'reconstructed.ascii_plot.txt'), 'wt') as fout:
+    fout.write(tree_reconstructed.as_ascii_plot())
+    fout.write('\n')
 
 # re-save the newwick after eliminating quotes around taxa labels
-tree.write_to_path(triplets_tree_path, schema='newick', unquoted_underscores=True)
+tree_reconstructed.write_to_path(triplets_tree_path, schema='newick', unquoted_underscores=True)
 
 # read simulation.newick
-tree2 = dendropy.Tree.get_from_path(
+tree_simulation = dendropy.Tree.get_from_path(
     os.path.join(work_path, 'simulation.newick'),
     "newick"
 )
 
-print(tree2.as_ascii_plot())
+with open(os.path.join(work_path, 'simulation.ascii_plot.txt'), 'wt') as fout:
+    fout.write(tree_simulation.as_ascii_plot())
+    fout.write('\n')
+
