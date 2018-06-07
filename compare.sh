4ff267946234a8ab662fa47a55a4caaa6ccfc04d
hisplan <jaeyoung.chun@gmail.com>
2018-06-07 08:59:05 +0300

    Add compare.sh

---
+++ compare.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+treecmp="./TreeCmp/bin/treeCmp.jar"
+
+#  Metrics for rooted trees
+metrics_r="mc rc ns tt mp mt co"
+
+# metrics for unrooted trees
+metrics_ur="ms rf pd qt um"
+
+java -jar ${treecmp} \
+  -r ./outputs/simulation.newick \
+  -d ${metrics_r} \
+  -i ./outputs/reconstructed.newick \
+  -o ./outputs/scores.out \
+  -P -N -I
