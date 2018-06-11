dd3e7505ec360510dbc28d90b6f156b16df4f3ae
hisplan <jaeyoung.chun@gmail.com>
2018-06-11 08:32:23 +0300

    Reorganize

--- test.py
+++ test.py
@@ -2,24 +2,33 @@ import os
 import subprocess
 
 def run_command(cmd):
+    "run command"
 
     process = subprocess.Popen(cmd)
 
     process.wait()
 
 
-path_matlab='/usr/wisdom/matlabR2017a/bin'
-path_matlab='/Applications/MATLAB_R2017b.app/bin'
+def main():
+    "main function"
 
-path_eSTGt='~/projects/eSTGt/eSTGt'
+    path_matlab='/usr/wisdom/matlabR2017a/bin'
+    path_matlab='/Applications/MATLAB_R2017b.app/bin'
 
-path_config='/Users/dchun/projects/clineage-simulation/src/config.json'
+    path_eSTGt='~/projects/eSTGt/eSTGt'
+
+    path_config='/Users/dchun/projects/clineage-simulation/simulation/config.json'
 
 
-cmd = [
-    os.path.join(path_matlab, 'matlab'),
-    '-nodisplay', '-nosplash', '-nodesktop',
-    '-r', "addpath('{}'); cd('src'); run_simul('{}'); exit;".format(path_eSTGt, path_config)
-]
+    cmd = [
+        os.path.join(path_matlab, 'matlab'),
+        '-nodisplay', '-nosplash', '-nodesktop',
+        '-r', "addpath('{}'); cd('src'); run_simul('{}'); exit;".format(path_eSTGt, path_config)
+    ]
 
-run_command(cmd)
+    run_command(cmd)
+
+
+if __name__ == "__main__":
+
+    main()
