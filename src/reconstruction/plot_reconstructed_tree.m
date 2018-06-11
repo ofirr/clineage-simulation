4da758fc614148607ec52ef82e52d4748369d27c
hisplan <jaeyoung.chun@gmail.com>
2018-06-11 16:01:21 +0300

    Reorder leaves in reconstructed tree to match leaf order in simulation tree

--- src/reconstruction/plot_reconstructed_tree.m
+++ src/reconstruction/plot_reconstructed_tree.m
@@ -1,13 +1,19 @@
-function [] = plot_reconstructed_tree(path_newick, path_png)
+function [] = plot_reconstructed_tree(path_simulation_newick, path_reconstructed_newick, path_png)
 
-% read from newick file
-reconstructed_tree = phytreeread(path_newick);
+% read simulation tree from newick file
+simulation_tree = phytreeread(path_simulation_newick);
+
+% read reconstructed tree from newick file
+reconstructed_tree = phytreeread(path_reconstructed_newick);
+
+% reorder leaves in reconstructed tree to match leaf order in simulation tree
+reconstructed_tree_reordered = reorder(reconstructed_tree, simulation_tree);
 
 % plot
-plot(reconstructed_tree);
+plot(reconstructed_tree_reordered);
 
 % set title
-title('Reconstructed');
+title('Reconstructed (Reordered)');
 
 % save to png
 saveas(gcf, path_png, 'png');
