15081dba5c3af85662be0070bd6be98cad554b05
hisplan <jaeyoung.chun@gmail.com>
2018-06-11 11:18:19 +0300

    Update

--- test.py
+++ test.py
@@ -3,6 +3,13 @@ import subprocess
 import json
 import argparse
 
+
+def read_json_config(path):
+
+    with open(path, 'rt') as file_in:
+        return json.loads(file_in.read())
+
+
 def run_command(cmd):
     "run command"
 
@@ -20,9 +27,11 @@ def simulate(path_matlab):
     path_working='./examples/example-01'
     path_working='./analysis/tmc'
 
+    config_filename = 'config-01.json'
+
     # run MATLAB simulation
     matlab_code = "addpath('{0}', '{1}', '{2}'); run_simul('{2}', '{3}'); exit;".format(
-        path_eSTGt, path_simul_lib, path_working, 'config-01.json'
+        path_eSTGt, path_simul_lib, path_working, config_filename
     )
 
     cmd = [
@@ -33,16 +42,21 @@ def simulate(path_matlab):
 
     run_command(cmd)
 
+    # read simulation configuration
+    config = read_json_config(os.path.join(path_working, config_filename)
+
+    path_simulation_output = os.path.join(path_working, config['pathRelativeOutput'])
+
     import dendropy
 
     # read simulation.newick
     tree = dendropy.Tree.get_from_path(
-        os.path.join(path_working, 'simulation.newick'),
+        os.path.join(path_simulation_output, 'simulation.newick'),
         "newick"
     )
 
     # write ascii plot for simulation tree
-    with open(os.path.join(path_working, 'simulation.ascii_plot.txt'), 'wt') as fout:
+    with open(os.path.join(path_simulation_output, 'simulation.ascii_plot.txt'), 'wt') as fout:
         fout.write(tree.as_ascii_plot())
         fout.write('\n')
 
@@ -61,9 +75,10 @@ def main():
 
     params = parser.parse_args()
 
-    with open(params.path_config_env, 'rt') as file_in:        
-        envs = json.loads(file_in.read())
+    # read environment configuration
+    envs = read_json_config(params.path_config)
 
+    # run simulation
     simulate(envs['matlab'])
 
 
