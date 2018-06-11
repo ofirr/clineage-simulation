01b609f118f29cc3e63814b3c2e50cb037fd4326
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2018-06-11 21:47:51 +0300

    Update

--- src/simulation/dev/simulation.m
+++ src/simulation/dev/simulation.m
@@ -2,10 +2,12 @@
 % change directory to ./src/simulation/dev first
 %
 
-addpath('C:\Users\chun\projects\eSTGt\eSTGt');
-% addpath('/Users/dchun/projects/eSTGt/eSTGt/');
+% addpath('C:\Users\chun\projects\eSTGt\eSTGt');
+addpath('/Users/dchun/projects/eSTGt/eSTGt/');
 
 addpath('..')
-addpath('../../../examples/example-01')
+% addpath('../../../examples/example-01')
+addpath('../../../analysis/tmc/')
 
-run_simul('../../../examples/example-01', 'config.json')
+% run_simul('../../../examples/example-01', 'config.json')
+run_simul('../../../analysis/tmc', 'config-04.json')
