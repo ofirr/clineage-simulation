2129afb6529b470616bab66cfe67cc6f1a90bb33
hisplan <jaeyoung.chun@gmail.com>
2018-06-11 16:18:37 +0300

    Add code that helps you dev

---
+++ src/simulation/dev/simulation.m
@@ -0,0 +1,11 @@
+%
+% change directory to ./src/simulation/dev first
+%
+
+addpath('C:\Users\chun\projects\eSTGt\eSTGt');
+% addpath('/Users/dchun/projects/eSTGt/eSTGt/');
+
+addpath('..')
+addpath('../../../examples/example-01')
+
+run_simul('../../../examples/example-01', 'config.json')
