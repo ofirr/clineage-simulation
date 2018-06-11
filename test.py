63203b903d8d97b9abb152f057ee86f7423add82
hisplan <jaeyoung.chun@gmail.com>
2018-06-11 11:43:37 +0300

    Update

--- test.py
+++ test.py
@@ -3,6 +3,14 @@ import subprocess
 import json
 import argparse
 
+ENV_MATLAB_KEY = 'matlab'
+
+FILE_SIMULATION_NEWICK = 'simulation.newick'
+FILE_RECONSTRUCTED_NEWICK = 'reconstructed.newick'
+FILE_TMC_LOG = 'tmc.log'
+FILE_MUTATION_TABLE = 'mutation_table.txt'
+
+CONFIG_PATH_RELATIVE_OUTPUT = 'pathRelativeOutput'
 
 def read_json_config(path):
 
@@ -16,6 +24,22 @@ def run_command(cmd):
     process = subprocess.Popen(cmd)
 
     process.wait()
+
+
+def generate_tree_ascii_plot(path_output, newick_filename):
+
+    import dendropy
+
+    # read simulation.newick
+    tree = dendropy.Tree.get_from_path(
+        os.path.join(path_output, newick_filename),
+        "newick"
+    )
+
+    # write ascii plot for simulation tree
+    with open(os.path.join(path_output, '{}.ascii-plot.txt'.format(newick_filename)), 'wt') as fout:
+        fout.write(tree.as_ascii_plot())
+        fout.write('\n')
 
 
 def simulate(path_matlab):
@@ -45,20 +69,80 @@ def simulate(path_matlab):
     # read simulation configuration
     config = read_json_config(os.path.join(path_working, config_filename))
 
-    path_simulation_output = os.path.join(path_working, config['pathRelativeOutput'])
+    path_simulation_output = os.path.join(path_working, config[CONFIG_PATH_RELATIVE_OUTPUT])
+
+    return path_simulation_output
+
+
+def reconstruct(path_simulation_output, root_cell_notation='root'):
+
+    import sys
+    sys.path.append("/home/chun/clineage/")
+    import clineage.wsgi
+
+    import os
+    from frogress import bar
+
+    # transpose dictionary
+    from sequencing.calling.queries.mutation_maps import transpose_dict
+
+    from sequencing.phylo.triplets_wrapper import get_cells_and_root, parse_mutations_table, run_sagis_triplets, run_sagis_triplets_binary
+
+    # construct path for input mutation table
+    path_mutation_table =  os.path.join(path_simulation_output, FILE_MUTATION_TABLE)
+
+    # parse mutation table
+    calling = parse_mutations_table(path_mutation_table, inverse=False)
+
+    # verify the presence of a root cell in the input data.
+    possible_roots = [cell for cell in calling if root_cell_notation in cell]
+    assert len(possible_roots) == 1
+    root_label = possible_roots[0]
+    root = (root_label, calling[root_label])
+    cells = []
+    for cell in calling:
+        if cell == root_label:
+            continue
+        cells.append((cell, calling[cell]))
+    assert len(cells) > 2
+
+    print(possible_roots)
+
+    print(cells)
+
+    # transpose
+    tcalling = transpose_dict(calling)
 
     import dendropy
 
-    # read simulation.newick
-    tree = dendropy.Tree.get_from_path(
-        os.path.join(path_simulation_output, 'simulation.newick'),
+    # run sagis triplets
+    rldr = [root_cell_notation] # cf. 'Ave'
+    triplets_tree_path = os.path.join(path_simulation_output, FILE_RECONSTRUCTED_NEWICK)
+
+    run_sagis_triplets(
+        textual_mutation_dict=tcalling,
+        cells_to_be_used_as_root=rldr,
+        newick_tree_path=triplets_tree_path,
+        tripletsnumber=5000000,
+        score_threshold=0,
+        scoring_method='uri10',
+        loci_filter='ncnr')
+
+    # fixme: would be nice if we can do this when calling `run_sagis_triplets`
+    os.rename("treeReconstruction.log", os.path.join(path_simulation_output, FILE_TMC_LOG))
+
+    # load reconstructed tree
+    tree_reconstructed = dendropy.Tree.get_from_path(
+        triplets_tree_path,
         "newick"
     )
 
-    # write ascii plot for simulation tree
-    with open(os.path.join(path_simulation_output, 'simulation.ascii_plot.txt'), 'wt') as fout:
-        fout.write(tree.as_ascii_plot())
-        fout.write('\n')
+    # prune root if necessary
+    # root_node = tree.find_node_with_taxon_label('root')
+    # tree.prune_subtree(root_node);
+
+    # re-save the newwick after eliminating quotes around taxa labels
+    tree_reconstructed.write_to_path(triplets_tree_path, schema='newick', unquoted_underscores=True)
 
 
 def main():
@@ -79,7 +163,16 @@ def main():
     envs = read_json_config(params.path_config_env)
 
     # run simulation
-    simulate(envs['matlab'])
+    path_simulation_output = simulate(envs[ENV_MATLAB_KEY])
+
+    # take simulation tree and make ascii plot
+    generate_tree_ascii_plot(path_simulation_output, FILE_SIMULATION_NEWICK)
+
+    # reconstruct based on mutation table generated from simulation
+    reconstruct(path_simulation_output)
+
+    # take reconstructed tree and make ascii plot
+    generate_tree_ascii_plot(path_simulation_output, FILE_RECONSTRUCTED_NEWICK)
 
 
 if __name__ == "__main__":
