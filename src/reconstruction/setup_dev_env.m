6181e24a26d482823db924280806e0cdb03d52de
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2018-06-12 21:23:33 +0300

    Minor update

--- src/reconstruction/setup_dev_env.m
+++ src/reconstruction/setup_dev_env.m
@@ -1,4 +1,5 @@
 path_working='../../analysis/tmc/case-04/';
+%path_working='../../examples/example-01/outputs';
 
 path_simulation_tree = fullfile(path_working, 'simulation.newick');
 path_reconstructed_tree = fullfile(path_working, 'reconstructed.newick');
