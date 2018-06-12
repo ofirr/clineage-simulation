524b3f015c22da47bbdfd1af0bd8a81f3ecc2409
hisplan <jaeyoung.chun@gmail.com>
2018-06-12 08:37:38 +0300

    Fix relative path

--- src/dev/test_reorder.m
+++ src/dev/test_reorder.m
@@ -1,10 +1,10 @@
 close all;
 
 % read simulation tree from newick file
-simulation_tree = phytreeread('../../../analysis/tmc/case-07/simulation.newick');
+simulation_tree = phytreeread('../../analysis/tmc/case-04/simulation.newick');
 
 % read reconstructed tree from newick file
-reconstructed_tree = phytreeread('../../../analysis/tmc/case-07/reconstructed.newick');
+reconstructed_tree = phytreeread('../../analysis/tmc/case-04/reconstructed.newick');
 
 % reorder leaves in reconstructed tree to match leaf order in simulation tree
 reconstructed_tree_reordered = reorder(reconstructed_tree, simulation_tree);
