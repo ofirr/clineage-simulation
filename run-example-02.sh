3c6f53175dccbb7d897a2325386133b93cc5e773
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2018-06-17 12:08:32 +0300

    Reorganize examples

---
+++ run-example-02.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+path_project='./examples/example-02'
+
+python run_end_to_end.py \
+  --env config.math102-lx.env \
+  --project ${path_project} \
+  --config config.json
+
+python make_html_report.py \
+  --project ${path_project} \
+  --config config.json
