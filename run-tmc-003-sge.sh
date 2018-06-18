c71a4dc2fae2dbeefdbb3a2f7cf03e6d2f142b64
hisplan <jaeyoung.chun@gmail.com>
2018-06-18 17:05:47 +0300

    Add TMC 003 SGE script

---
+++ run-tmc-003-sge.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+path_project="/home/chun/projects/clineage-simulation/analysis/tmc-003/"
+
+for i in {1..20}
+do
+    num=`printf "%02d\n" $i`
+	./sge-submit.sh \
+		-p ${path_project} \
+		-c config-${num}.json
+done
