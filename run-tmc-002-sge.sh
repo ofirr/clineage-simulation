1b0acab85118f95def35f376a34341d17e797987
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2018-06-18 15:27:15 +0300

    Revise

--- run-tmc-002-sge.sh
+++ run-tmc-002-sge.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-./sge-submit.sh \
-  -p /home/chun/projects/clineage-simulation/examples/example-02/ \
-  -c config.json
+path_project="/home/chun/projects/clineage-simulation/analysis/tmc-002/"
 
-
+for i in {1..20}
+do
+    num=`printf "%02d\n" $i`
+	./sge-submit.sh \
+		-p ${path_project} \
+		-c config-${num}.json
+done
