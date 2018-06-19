34608796d40dbbcd140ff0d11deb8783e8aa150a
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2018-06-19 10:28:29 +0300

    Revise

--- analysis/total_score.py
+++ analysis/total_score.py
@@ -8,7 +8,9 @@ def run(seed):
 
     for sim_no in range(1, 15):
 
-        list = glob.glob("./tmc-{0}/tmc-{1:03d}/*/diff-score.json".format(seed, sim_no))
+        list = glob.glob(
+            "./tmc-{0}/tmc-{1:03d}/*/diff-score.json".format(seed, sim_no)
+        )
 
         global_total = 0
         global_same = 0
@@ -22,7 +24,8 @@ def run(seed):
                 global_same += same
 
         ratio = global_same / global_total
-        print("{0}\ttmc-{1:03d}\t{2:1.3f}\t{3:2.1f}%".format(seed, sim_no, ratio, ratio * 100.0))
+        print("{0}\ttmc-{1:03d}\t{2:1.3f}\t{3:2.1f}%".format(seed,
+                                                             sim_no, ratio, ratio * 100.0))
 
 
 def parse_arguments():
@@ -33,7 +36,8 @@ def parse_arguments():
         "--seed",
         action="store",
         dest="seed",
-        required=True
+        required=True,
+        help="Either a single seed number or 'all' for all seeds (e.g. tmc-123, tmc-456, ...)"
     )
 
     # parse arguments
@@ -50,11 +54,11 @@ if __name__ == "__main__":
         projects = glob.glob('./tmc-*')
         for prj in projects:
             match = re.search(r'tmc-(\d+)', prj)
-            if match:
-                seed = match.group(1)
-                run(seed)
-                print()
+            if not match:
+                continue
+            seed = match.group(1)
+            run(seed)
+            print()
 
     else:
         run(params.seed)
-
