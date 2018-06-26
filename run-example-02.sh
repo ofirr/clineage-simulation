aa06892bf6d4a6e8302f5025914d8952321ad644
hisplan <jaeyoung.chun@gmail.com>
2018-06-26 08:02:40 +0300

    Revise example-02

--- run-example-02.sh
+++ run-example-02.sh
@@ -5,8 +5,8 @@ path_project='./examples/example-02'
 python run_end_to_end.py \
   --env config.math102-lx.env \
   --project ${path_project} \
-  --config config.json
+  --config config.list
 
 python make_html_report.py \
   --project ${path_project} \
-  --config config.json
+  --config config.list
