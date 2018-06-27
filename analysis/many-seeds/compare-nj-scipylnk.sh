081d0211a2af017eed6e69fa0d170f4595541441
hisplan <jaeyoung.chun@gmail.com>
2018-06-27 10:37:43 +0300

    Fix

--- analysis/many-seeds/compare-nj-scipylnk.sh
+++ analysis/many-seeds/compare-nj-scipylnk.sh
@@ -5,36 +5,36 @@ usage()
 cat << EOF
 USAGE: `basename $0` [options]
 
-    -r    root directory
+    -p    project directory
     -o    output base directory
 
 EOF
 }
 
-while getopts "r:o:h" OPTION
+while getopts "p:o:h" OPTION
 do
     case $OPTION in
-        r) path_root=$OPTARG ;;
+        p) path_project=$OPTARG ;;
         o) path_output_base=$OPTARG ;;
         h) usage; exit 1 ;;
         *) usage; exit 1 ;;
     esac
 done
 
-if [ -z "$path_root" ] || [ -z "$path_output_base" ]
+if [ -z "$path_project" ] || [ -z "$path_output_base" ]
 then
     usage
     exit 1
 fi
 
-seeds=`find ${path_root} -name "seed-*" -type d`
+seeds=`find ${path_project} -name "seed-*" -type d`
 
-for path_project in $seeds
+for seed_dir in $seeds
 do
 
-    seed=`echo ${path_project} | awk -F'/' '{print $(NF)}' | cut -c6-`
+    seed=`echo ${seed_dir} | awk -F'/' '{print $(NF)}' | cut -c6-`
 
-    echo ${path_project}
+    echo ${seed_dir}
 
     for ii in {1..5}
     do
