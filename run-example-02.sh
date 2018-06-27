aded8fc4261b25422c841d01341e3f9392c83bb0
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2018-06-27 10:14:56 +0300

    Rerun example-02, 02a, 02b

--- run-example-02.sh
+++ run-example-02.sh
@@ -5,8 +5,9 @@ path_project='./examples/example-02'
 python run_end_to_end.py \
   --env config.math102-lx.env \
   --project ${path_project} \
-  --config config.list
+  --config config.json
 
 python make_html_report.py \
   --project ${path_project} \
-  --config config.list
+  --config config.json
+
