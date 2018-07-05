96e0f26b4d9051ab225505c1625afd1b41222b30
hisplan <jaeyoung.chun@gmail.com>
2018-07-05 16:47:12 +0300

    Add adoFixedThreshold and mutationNoiseThreshold

--- src/simulation/generate_ado_truth_table.m
+++ src/simulation/generate_ado_truth_table.m
@@ -1,4 +1,4 @@
-function [ado_truth_table] = generate_ado_truth_table(num_of_samples, num_of_ms_loci, dropout_prob)
+function [ado_truth_table] = generate_ado_truth_table(num_of_samples, num_of_ms_loci, dropout_prob, threshold)
 
 % dropout_prob.P = the probability of obtaining a signal in each sample
 % dropout_prob.Q = = the probability of obtaining a signal in each locus
@@ -10,7 +10,9 @@ ms_loci_idx = randi([1 length(dropout_prob.Q)], 1, num_of_ms_loci);
 Q = dropout_prob.Q(ms_loci_idx);
 
 % generate threshold values (randomized)
-threshold = rand(num_of_ms_loci, num_of_samples);
+if isnan(threshold)
+    threshold = rand(num_of_ms_loci, num_of_samples);
+end
 
 % compute signal values
 signal_prob = Q'*P;
