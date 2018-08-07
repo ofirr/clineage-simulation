d9f43d39d930d81fedf011e3ca20242ba9808262
hisplan <jaeyoung.chun@gmail.com>
2018-08-07 11:07:21 +0300

    Run all examples
    Former-commit-id: 13269ba78a0009b6d0b7ce3d229d46208031ca7b

---
+++ src/run_all_examples.m
@@ -0,0 +1,31 @@
+clear;
+clc;
+
+%% code path
+
+addpath('./reconstruction');
+addpath('./simulation');
+
+%% path to eSTGt (Windows)
+
+addpath('C:\Users\chun\projects\eSTGt\eSTGt');
+
+%% run all
+
+list = readtable('./all-examples.csv', 'Delimiter', ',');
+
+for row = 1:size(list, 1)
+    
+    path_working = list.path_working{row};
+    config_file = list.config_file{row};
+    
+    fprintf('%-50s %s\n', path_working, config_file);
+  
+    addpath(path_working);
+    
+    run_simul(path_working, config_file);
+      
+    rmpath(path_working);
+end
+
+close all;
