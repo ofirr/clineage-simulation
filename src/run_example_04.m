8add908218023570ca303d41c3237a86d149269e
hisplan <jaeyoung.chun@gmail.com>
2018-08-07 11:23:39 +0300

    Update unit test

--- src/run_example_04.m
+++ src/run_example_04.m
@@ -1,5 +1,5 @@
-run1 = load(fullfile(path_working, 'outputs-all-the-way', 'workspace.mat'), 'my_run');
-run2 = load(fullfile(path_working, 'outputs-early-stop', 'workspace.mat'), 'my_run');
+run1 = load(fullfile('../examples/example-early-stop-verification/', 'outputs-all-the-way', 'workspace.mat'), 'my_run');
+run2 = load(fullfile('../examples/example-early-stop-verification/', 'outputs-early-stop', 'workspace.mat'), 'my_run');
 
 [result, reasons] = are_runs_identical(run1.my_run, run2.my_run);
 
