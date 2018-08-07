24127958025a7896fad7c1538f987521342e5683
hisplan <jaeyoung.chun@gmail.com>
2018-08-07 11:31:18 +0300

    Minor update
    Former-commit-id: 41afd497c4b3e0d7b3d705196ac75e5b2e8a70bc

--- src/run_all_examples.m
+++ src/run_all_examples.m
@@ -13,13 +13,14 @@ addpath('C:\Users\chun\projects\eSTGt\eSTGt');
 
 list = readtable('./all-examples.csv', 'Delimiter', ',');
 
-for row = 17:size(list, 1)
+for row = 1:size(list, 1)
     
     path_working = list.path_working{row};
     config_file = list.config_file{row};
     
-    fprintf('\n---------------------------------------------------------------\n');
-    fprintf('%-50s %s\n', path_working, config_file);
+    fprintf('\n---------------------------------------------------------\n');
+    fprintf('%s:%s\n', path_working(13:end), config_file);
+    fprintf('---------------------------------------------------------\n');
   
     addpath(path_working);
     
@@ -29,3 +30,5 @@ for row = 17:size(list, 1)
 end
 
 close all;
+
+disp('DONE.');
