eaee89f0e49eb2bf1e174c7d6a86590c6350d523
hisplan <jaeyoung.chun@gmail.com>
2018-08-22 16:44:45 +0300

    Check qsub availability

--- run-tmc-dev-sge.sh
+++ run-tmc-dev-sge.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ ! -x "$(command -v qsub)" ]
+then
+    echo "qsub not found. Aborted."
+	exit 1
+fi
+
 path_project="/home/chun/projects/clineage-simulation/analysis/tmc-dev"
 
 for i in {1..20}
