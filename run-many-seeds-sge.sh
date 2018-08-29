cea11cb61769fdc00b4622d659f62b1f45352656
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2018-08-29 14:37:09 +0300

    Improve SGE interface
    Former-commit-id: 6d5a1f600eb4ad9b44bd41413c5de6f595557555

--- run-many-seeds-sge.sh
+++ run-many-seeds-sge.sh
@@ -3,7 +3,7 @@
 if [ ! -x "$(command -v qsub)" ]
 then
     echo "qsub not found. Aborted."
-	exit 1
+    exit 1
 fi
 
 usage()
@@ -22,11 +22,11 @@ simulate_tree_only='';
 while getopts "r:sh" OPTION
 do
     case $OPTION in
-		r) path_root=$OPTARG ;;
+        r) path_root=$OPTARG ;;
         s) simulate_tree_only='-s' ;;
-		h) usage; exit 1 ;;
-		*) usage; exit 1 ;;
-	esac
+        h) usage; exit 1 ;;
+        *) usage; exit 1 ;;
+    esac
 done
 
 if [ -z "$path_root" ]
@@ -37,6 +37,10 @@ fi
 
 seeds=`find ${path_root} -maxdepth 1 -name "seed-*" -type d`
 
+project_name=$(uuidgen)
+
+echo ">>> Project Name: ${project_name}"
+
 for path_project in $seeds
 do
 
@@ -44,8 +48,9 @@ do
 
     for case in $cases
     do
-       echo "${path_project}/${case}"
-       ./sge-submit.sh \
+        echo "${path_project}/${case}"
+        ./sge-submit.sh \
+            -n ${project_name} \
             -p ${path_project} \
             -c ${case} ${simulate_tree_only}
     done
--- sge-submit.sh
+++ sge-submit.sh
@@ -3,32 +3,37 @@
 if [ ! -x "$(command -v qsub)" ]
 then
     echo "qsub not found. Aborted."
-	exit 1
+    exit 1
 fi
+
+# default project name
+project_name='simulation'
 
 usage()
 {
 cat << EOF
 USAGE: `basename $0` [options]
 
-	-p	path to project (e.g. ./examples/example-02)
-	-c	config file (e.g. config.json)
-	-s	simulate tree only (no reconstruction)
+    -p	path to project (e.g. ./examples/example-02)
+    -c	config file (e.g. config.json)
+    -s	simulate tree only (no reconstruction)
+    -n	project name (default: "${project_name}")
 
 EOF
 }
 
 simulate_tree_only='';
 
-while getopts "p:c:sh" OPTION
+while getopts "n:p:c:sh" OPTION
 do
     case $OPTION in
-		p) path_project=$OPTARG ;;
-		c) config_file=$OPTARG ;;
-		s) simulate_tree_only='--simulate-tree-only' ;;
-		h) usage; exit 1 ;;
-		*) usage; exit 1 ;;
-	esac
+        n) project_name=$OPTARG ;;
+        p) path_project=$OPTARG ;;
+        c) config_file=$OPTARG ;;
+        s) simulate_tree_only='--simulate-tree-only' ;;
+        h) usage; exit 1 ;;
+        *) usage; exit 1 ;;
+    esac
 done
 
 if [ -z "$path_project" ] || [ -z "$config_file" ]
@@ -37,8 +42,20 @@ then
     exit 1
 fi
 
-qsub \
-	-N simulation \
-	-q all2.q \
-	-cwd \
-	./sge-run-simul.sh ${path_project} ${config_file} ${simulate_tree_only}
+# set unique job name
+job_name="job-`uuidgen`"
+
+out=`qsub \
+    -N ${job_name} \
+    -q all2.q \
+    -cwd \
+    ./sge-run-simul.sh ${path_project} ${config_file} ${simulate_tree_only}`
+
+# extract SGE job ID from the qsub output
+# e.g. Your job 7588748 ("simulation") has been submitted
+job_id=`echo ${out} | awk -F' ' '{print $3}'`
+
+echo ${job_id}
+
+# record
+echo "${job_id},${path_project},${config_file}" >> ${project_name}.job-info
