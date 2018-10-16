a6a6a4744aa15ffa1dbf0aad98938442897b24b5
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2018-10-16 15:37:13 +0300

    Remove unnecessary

--- analysis/many-seeds/template-1K-100ish-monoallelic-diffdist.001.with_mono/update_microsatellite_2.m
+++
@@ -1,9 +0,0 @@
-function [ newMS ] = update_microsatellite_2( MS )
-    % updates the microsatellite repeat number
-    
-        % 1 for paternal
-        % 2 for maternal
-        newMS = update_microsatellite(MS, 2);
-    
-    end
-    
