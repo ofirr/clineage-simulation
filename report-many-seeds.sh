94cbdc502e5306243fa82b56e190cd8d4e3c0374
hisplan <jaeyoung.chun@gmail.com>
2018-10-17 12:43:58 +0300

    Remove hardcoded path

--- report-many-seeds.sh
+++ report-many-seeds.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-path_root='/home/chun/projects/clineage-simulation/analysis/many-seeds/'
+usage()
+{
+cat << EOF
+USAGE: `basename $0` [options]
+
+    -r  absolute path to simulation root where seed-* are placed
+
+EOF
+}
+
+while getopts "r:h" OPTION
+do
+    case $OPTION in
+        r) path_root=$OPTARG ;;
+        h) usage; exit 1 ;;
+        *) usage; exit 1 ;;
+    esac
+done
+
+if [ -z "$path_root" ]
+then
+    usage
+    exit 1
+fi
 
 seeds=`find ${path_root} -maxdepth 1 -name "seed-*" -type d`
 
